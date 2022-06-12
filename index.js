1e3e5a51a35662bc341ce1a92e7fb4d7560081d2
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-12 17:16:06 +0300

    Tested the function locally

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ let superbowlWin = (arr)=> {
     else return winner;
  }   
 
- 
+ const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+];
+
+console.log(superbowlWin(record));
